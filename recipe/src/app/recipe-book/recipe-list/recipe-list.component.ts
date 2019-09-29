7581875694afe97176bdd539eeceee3f3d261f01
ryanlukecross <cro16052@byui.edu>
2019-09-28 19:00:01 -0600

    finished with demo videos for recipes, switching to homework

--- recipe/src/app/recipe-book/recipe-list/recipe-list.component.ts
+++ recipe/src/app/recipe-book/recipe-list/recipe-list.component.ts
@@ -1,12 +1,16 @@
 import { Component, OnInit } from '@angular/core';
 
+import { Recipe } from '../recipe.model';
+
 @Component({
   selector: 'app-recipe-list',
   templateUrl: './recipe-list.component.html',
   styleUrls: ['./recipe-list.component.css']
 })
 export class RecipeListComponent implements OnInit {
-
+  recipes: Recipe[] = [
+     new Recipe("Test Recipe 1", "This is the first test recipe", "https://www.jessicagavin.com/wp-content/uploads/2018/06/how-to-reverse-sear-a-steak-11.jpg")
+  ];
   constructor() { }
 
   ngOnInit() {
