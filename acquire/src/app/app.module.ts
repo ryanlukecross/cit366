f466a2fa84eabfcef4c381c0937ad4ce989bc3f1
ryanlukecross <cro16052@byui.edu>
2019-12-10 22:38:30 -0700

    Added components (admin/player-list/player-details)

--- acquire/src/app/app.module.ts
+++ acquire/src/app/app.module.ts
@@ -3,16 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PlayerListComponent } from './admin/player-list/player-list.component';
+import { PlayerDetailComponent } from './admin/player-list/player-detail/player-detail.component';
+import { AdminComponent } from './admin/admin.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
-  providers: [],
-  bootstrap: [AppComponent]
+   declarations: [
+      AppComponent,
+      PlayerListComponent,
+      PlayerDetailComponent,
+      AdminComponent
+   ],
+   imports: [
+      BrowserModule,
+      AppRoutingModule
+   ],
+   providers: [],
+   bootstrap: [AppComponent]
 })
 export class AppModule { }
