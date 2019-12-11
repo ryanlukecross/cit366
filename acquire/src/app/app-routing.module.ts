178107a61849dfd502953231e85dcb806dd59aae
ryanlukecross <cro16052@byui.edu>
2019-12-10 22:53:45 -0700

    routing works

--- acquire/src/app/app-routing.module.ts
+++ acquire/src/app/app-routing.module.ts
@@ -1,11 +1,30 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { AdminComponent } from './admin/admin.component';
+import { PlayerDetailComponent } from './admin/player-list/player-detail/player-detail.component';
+import { PlayerListComponent } from './admin/player-list/player-list.component';
+import { PlayerEditComponent } from './admin/player-list/player-edit/player-edit.component';
 
 
-const routes: Routes = [];
+
+const routes: Routes = [
+   { path: '', redirectTo: '/admin', pathMatch: 'full' },
+   {
+      path: 'admin', component: AdminComponent, children: [
+         {
+            path: 'players', component: PlayerListComponent, children: [
+               { path: 'new', component: PlayerEditComponent },
+               { path: ':id', component: PlayerDetailComponent },
+               { path: ':id/edit', component: PlayerEditComponent }
+            ]
+         },
+
+      ]
+   }
+];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+   imports: [RouterModule.forRoot(routes)],
+   exports: [RouterModule]
 })
 export class AppRoutingModule { }
--- acquire/src/app/app.module.ts
+++ acquire/src/app/app.module.ts
@@ -6,13 +6,15 @@ import { AppComponent } from './app.component';
 import { PlayerListComponent } from './admin/player-list/player-list.component';
 import { PlayerDetailComponent } from './admin/player-list/player-detail/player-detail.component';
 import { AdminComponent } from './admin/admin.component';
+import { PlayerEditComponent } from './admin/player-list/player-edit/player-edit.component';
 
 @NgModule({
    declarations: [
       AppComponent,
       PlayerListComponent,
       PlayerDetailComponent,
-      AdminComponent
+      AdminComponent,
+      PlayerEditComponent
    ],
    imports: [
       BrowserModule,
